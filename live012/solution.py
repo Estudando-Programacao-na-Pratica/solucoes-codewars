c3a325757c3868091de71099009ef5c06e359360
olv-leo <1996olv.leo@gmail.com>
2022-03-25 09:36:40 -0300

    Solução final inicial

--- live012/solution.py
+++ live012/solution.py
@@ -1,16 +1,10 @@
 def separar_numeros(string):
     temp = '-' if string[0] == '-' else ''
+    string = string.replace('--', '+') if '--' in string else string[1:] if temp == '-' else string
     op = ''
     resp = []
 
-    if '--' in string:
-        string = string.replace('--', '+')
-
-    if temp == '-':
-        string = string[1:]
-
     for numero in string:
-        # Acionado a condição final para lidar com o caso de -5*-5=5?
         if numero.isalnum() or numero == '?' or (numero == '-' and op != ''):
             temp += numero
         else:
@@ -18,40 +12,36 @@ def separar_numeros(string):
             temp = ''
             if not op:
                 op = numero
-
     resp.append(temp)
     resp.append(op)
-
     return resp
+
+
+def pode_zero(num):
+    for i in range(3):
+        primeiro_caracter = num[i][1] if num[i][0] == '-' else num[i][0]
+        if (str(num[i]).replace('?', '') == '' or primeiro_caracter == '?') and len(num[i]) > 1:
+            return False
+    return True
 
 
 def solve_runes(runes):
     num_separados = separar_numeros(runes)
 
-    for num in range(10):
-        num1 = int(num_separados[0].replace('?', str(num)))
-        num2 = int(num_separados[1].replace('?', str(num)))
-        resultado = int(num_separados[2].replace('?', str(num)))
-
-        if num == 0:
-            if str(num_separados[0]).replace('?', '') == '' and len(num_separados[0]) > 1:
-                continue
-
-            if str(num_separados[1]).replace('?', '') == '' and len(num_separados[1]) > 1:
-                continue
-
-            if str(num_separados[2]).replace('?', '') == '' and len(num_separados[2]) > 1:
-                continue
-
+    for num in range(1, 10) if not pode_zero(num_separados) else range(10):
         if str(num) in runes:
             continue
 
-        if num_separados[-1] == '+' and num1 + num2 == resultado:
+        num1 = int(num_separados[0].replace('?', str(num)))
+        num2 = int(num_separados[1].replace('?', str(num)))
+        resultado = int(num_separados[2].replace('?', str(num)))
+        operacoes = {
+            '+': num1 + num2 == resultado,
+            '-': num1 - num2 == resultado,
+            '*': num1 * num2 == resultado,
+        }
+
+        if operacoes[num_separados[-1]]:
             return num
 
-        if num_separados[-1] == '-' and num1 - num2 == resultado:
-            return num
-
-        if num_separados[-1] == '*' and num1 * num2 == resultado:
-            return num
     return -1
--- live012/tests/test_solve_runes.py
+++ live012/tests/test_solve_runes.py
@@ -3,7 +3,6 @@ from live012.solution import solve_runes
 
 def test_solve_runes():
     assert solve_runes("1+1=?") == 2
-    assert solve_runes("1+1=?") == 2
     assert solve_runes("123*45?=5?088") == 6
     assert solve_runes("-5?*-1=5?") == 0
     assert solve_runes("19--45=5?") == -1
